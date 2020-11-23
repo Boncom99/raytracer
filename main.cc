aa6c1f8a4c8ca3136bd510da43babc226e2ac9ac
Josep Boncompte <josepboncompte@MacBook-Pro.local>
2020-11-24 00:56:40 +0100

    square

--- main.cc
+++ main.cc
@@ -25,97 +25,36 @@ color ray_color(const ray& r, const hittable& world, int depth) {
 }
 
 
-hittable_list random_scene() {
+hittable_list random_scene(int n) {
     hittable_list world;
     shared_ptr<material> sphere_material;
-      double r =10000;
-    auto ground_material = make_shared<lambertian>(color(1, 0.2, 0.2));
+      double r =500;
+    auto ground_material = make_shared<lambertian>(color(1, 0.7, 0.5));
     world.add(make_shared<sphere>(point3(0,-r,0), r, ground_material));
 
-    point3 bola(1,1+sqrt(2),-2);
-    vec3 vbola1(-1,-sqrt(2),+1);
-    vec3 vbola2(1,-sqrt(2),+1);
-    vec3 vbola3(1,-sqrt(2),-1);
-    vec3 vbola4(-1,-sqrt(2),-1);
-
-    point3 bola1= bola+ vbola1;
-    point3 bola2= bola+ vbola2;
-    point3 bola3= bola+ vbola3;
-    point3 bola4= bola+ vbola4;
-
     auto glass = make_shared<dielectric>(1.5);
-    auto metall = make_shared<metal>(color(0.7, 0.7, 0.5), 0.0);
+    auto metall = make_shared<metal>(color(0.6, 0.6, 0.6), 0.0);
 
-    for (int a = -2; a < 2; a++) {
-        for (int b = -2; b < 2; b++) {
+     for(int y =0;y<n; y++){
+        for (int z = 0; z > -n; z--) {
+          for (int x = 0; x < n; x++) {
 
-                //point3 bola(9*a + 0.9*random_double(), 1+sqrt(2) -(r-sqrt(abs(r^2-z^2))),z);
-                point3 bola(9*a + 0.9*random_double(), 1+sqrt(2) ,9*b+ 0.9*random_double());
-                  point3 bola1= bola+ vbola1;
-                  point3 bola2= bola+ vbola2;
-                  point3 bola3= bola+ vbola3;
-                  point3 bola4= bola+ vbola4;
+                point3 bola(x,y+0.5,z-3);
           
-                auto rand = random_double();
-                sphere_material = make_shared<lambertian>(color(1,0.7,random_double() ));
-                if(rand <=1.0/3.0){
-                world.add(make_shared<sphere>(bola1, 1.0,  glass ));
-                }
-                else if (rand>1.0/3.0 && rand<=2.0/3.0){
-                world.add(make_shared<sphere>(bola1, 1.0,metall));
-                }
-                else {
-                    world.add(make_shared<sphere>(bola1, 1.0,sphere_material));
-                }
+              //  auto rand = random_double();
+              //  sphere_material = make_shared<lambertian>(color(1,0.7,random_double() ));
+             //   if(rand <=1.0/3.0){
+             //   world.add(make_shared<sphere>(bola, 0.5,  glass ));
+             //   }
+            //    if (rand>0.5){
+                world.add(make_shared<sphere>(bola, 0.5,metall));
+            //    }
+            //    else {
+            //        world.add(make_shared<sphere>(bola, 0.5,sphere_material));
+            //    }
 
-                rand = random_double();
-                if(rand <=1.0/3.0){
-                world.add(make_shared<sphere>(bola2, 1.0,  glass ));
-                }
-                else if (rand>1.0/3.0 && rand<=2.0/3.0){
-                world.add(make_shared<sphere>(bola2, 1.0,metall));
-                }
-                else {
-                    world.add(make_shared<sphere>(bola2, 1.0,sphere_material));
-                }
- 
-                rand = random_double();
-                sphere_material = make_shared<lambertian>(color(1,0.7,random_double() ));
-                if(rand <=1.0/3.0){
-                world.add(make_shared<sphere>(bola3, 1.0,  glass ));
-                }
-                else if (rand>1.0/3.0 && rand<=2.0/3.0){
-                world.add(make_shared<sphere>(bola3, 1.0,metall));
-                }
-                else {
-                    world.add(make_shared<sphere>(bola3, 1.0,sphere_material));
-                }
-                rand = random_double();
-                if(rand <=1.0/3.0){
-                world.add(make_shared<sphere>(bola4, 1.0,  glass ));
-                }
-                else if (rand>1.0/3.0 && rand<=2.0/3.0){
-                world.add(make_shared<sphere>(bola4, 1.0,metall));
-                }
-                else {
-                    world.add(make_shared<sphere>(bola4, 1.0,sphere_material));
-                }
-                rand = random_double();
-                if(rand <=1.0/3.0){
-                world.add(make_shared<sphere>(bola, 1.0,  glass ));
-                }
-                else if (rand>1.0/3.0 && rand<=2.0/3.0){
-                world.add(make_shared<sphere>(bola, 1.0,metall));
-                }
-                else {
-                    world.add(make_shared<sphere>(bola, 1.0,sphere_material));
-                }
-
- 
- 
- 
-
-                    }
+        }
+    }
     }
     
     return world;
@@ -125,18 +64,19 @@ int main() {
 
     // Image
 
-    const auto aspect_ratio = 3.0 / 2.0;
-    const int image_width = 800;
+    const auto aspect_ratio = 2.0 / 3.0;
+    const int image_width = 1200;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
-    const int samples_per_pixel=300; //100
+    const int samples_per_pixel=500; //100
     const int max_depth=50; //max number of bounces of a ray
 
     // World
-    auto world = random_scene();
+    int n =4;
+    auto world = random_scene(n);
 
     // Camera
-    point3 lookfrom(14,4,8);
-    point3 lookat(1,1,-2);
+    point3 lookfrom(n-1+3 + 4*n,6+n,4*n);
+    point3 lookat(n-1,n-0.5,-3);
     vec3 vup(0,1,0);
     auto dist_to_focus = 12.0;
     auto aperture = 0.0001; //small -> landscape , big ->small and close object       
