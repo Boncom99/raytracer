52c1f4e0ec068a8b7405c5383ae560d372c66f58
Josep Boncompte <josepboncompte@MacBook-Pro.local>
2020-11-23 15:58:54 +0100

    next week

--- main.cc
+++ main.cc
@@ -78,7 +78,7 @@ int main() {
     // Image
 
     const auto aspect_ratio = 3.0 / 2.0;
-    const int image_width = 1200;
+    const int image_width = 1300;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
     const int samples_per_pixel=500; //100
     const int max_depth=50; //max number of bounces of a ray
