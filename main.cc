f9a5b27229da2e581caba51571d05fde37aa2358
Josep Boncompte <josepboncompte@MacBook-Pro.local>
2020-11-25 20:17:04 +0100

    earth image

--- main.cc
+++ main.cc
@@ -26,6 +26,15 @@ color ray_color(const ray &r, const hittable &world, int depth)
     auto t = 0.5 * (unit_direction.y() + 1.0);
     return (1 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
+hittable_list earth() {
+    auto earth_texture = make_shared<image_texture>("external/image.jpg");
+    auto earth_surface = make_shared<lambertian>(earth_texture);
+    auto globe = make_shared<sphere>(point3(0,0,0), 2, earth_surface);
+
+    return hittable_list(globe);
+}
+
+
 hittable_list two_spheres()
 {
     hittable_list objects;
@@ -138,13 +147,19 @@ int main()
         lookat = point3(0, 0, 0);
         vfov = 20.0;
         break;
-    default:
     case 3:
         world = two_perlin_spheres();
         lookfrom = point3(13, 2, 3);
         lookat = point3(0, 0, 0);
         vfov = 20.0;
         break;
+    default:
+         case 4:
+            world = earth();
+            lookfrom = point3(13,2,3);
+            lookat = point3(0,0,0);
+            vfov = 20.0;
+            break;
     }
 
     // Camera
